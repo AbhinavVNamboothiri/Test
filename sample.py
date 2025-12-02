2d3793634b1257fb8440778de94a4dcb8c8a400d
AbhinavVNamboothiri <abhinavvnamboothiri@gmail.com>
2025-12-02 15:47:55 +0530

    Implement OpenAI client and response generation in sample.py

--- sample.py
+++ sample.py
@@ -1,1 +1,15 @@
-print("Hellow world")
+
+from openai import OpenAI
+import os
+client = OpenAI(
+    api_key="api_key_value",
+    base_url="https://api.groq.com/openai/v1",
+)
+
+response = client.responses.create(
+    input="Who is the pm of india?",
+    model="openai/gpt-oss-20b",
+    temperature=0.1,
+    max_output_tokens=1000,
+)
+print(response.output_text)
